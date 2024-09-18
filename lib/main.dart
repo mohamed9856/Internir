8bcac9420ee77caebc7d4ef7d6f560ce5a947e81
abdo1234510 <92454005+Abdelrahman-Wael-1029@users.noreply.github.com>
2024-09-18 19:17:47 +0300

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:internir/utils/size_config.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );  
   runApp(const MyApp());
 }
 
@@ -10,6 +17,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SizeConfig().init(context);
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
