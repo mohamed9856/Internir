c832aa523d84f29f9f116bfda97e4ef4905c31fb
mohamed9856 <129060392+mohamed9856@users.noreply.github.com>
2024-10-08 17:22:38 +0300

    adding ApplyScreen

---
+++ lib/screens/apply/apply_screen.dart
@@ -0,0 +1,20 @@
+// import 'package:flutter/material.dart';
+//
+// class ApplyScreen extends StatefulWidget {
+//   const ApplyScreen({super.key});
+//
+//   @override
+//   State<ApplyScreen> createState() => _ApplyScreenState();
+// }
+//
+// class _ApplyScreenState extends State<ApplyScreen> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Column(
+//       mainAxisAlignment: MainAxisAlignment.center,
+//       children: [
+//         Image.asset(name)
+//       ],
+//     );
+//   }
+// }
