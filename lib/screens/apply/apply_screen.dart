64348508c450d28270aabbadc25fafe7c826f2e8
mohamed9856 <129060392+mohamed9856@users.noreply.github.com>
2024-10-15 17:19:29 +0300

    apply list

--- lib/screens/apply/apply_screen.dart
+++ lib/screens/apply/apply_screen.dart
@@ -1,20 +1,120 @@
-// import 'package:flutter/material.dart';
-//
-// class ApplyScreen extends StatefulWidget {
-//   const ApplyScreen({super.key});
-//
-//   @override
-//   State<ApplyScreen> createState() => _ApplyScreenState();
-// }
-//
-// class _ApplyScreenState extends State<ApplyScreen> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Column(
-//       mainAxisAlignment: MainAxisAlignment.center,
-//       children: [
-//         Image.asset(name)
-//       ],
-//     );
-//   }
-// }
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class ApplyScreen extends StatelessWidget {
+  final String jobId;
+
+  const ApplyScreen({super.key, required this.jobId});
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<DocumentSnapshot>(
+      future: FirebaseFirestore.instance.collection('jobs').doc(jobId).get(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator(),);
+        }
+        if (!snapshot.hasData || !snapshot.data!.exists) {
+          return const Center(child: Text('Job not found'),);
+        }
+
+        var jobData = snapshot.data!.data() as Map<String, dynamic>;
+
+        String formattedDate = jobData['createdAt'] != null
+            ? DateFormat.yMMMMd().add_jm().format(jobData['createdAt'].toDate())
+            : 'No date';
+
+        return Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Center(
+                  child: Column(
+                    children: [
+                      Text(
+                        jobData['title'] ?? 'No title',
+                        style: const TextStyle(
+                            fontSize: 24, fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(height: 4),
+                      Text(
+                        jobData['company'] ?? 'No company',
+                        style: const TextStyle(fontSize: 16, color: Colors.grey,),
+                      ),
+                      const SizedBox(height: 4),
+                      Text(
+                        'Salary: \$${jobData['salary']?.toString() ?? 'N/A'}',
+                        style: const TextStyle(fontSize: 16, color: Colors.grey,),
+                      ),
+                      const SizedBox(height: 4),
+                      Text(
+                        jobData['category'] ?? 'No category',
+                        style: const TextStyle(fontSize: 16, color: Colors.grey,),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 24),
+
+                _buildSectionTitle('Job Description:'),
+                Text(
+                  jobData['description'] ?? 'No description available',
+                  style: const TextStyle(fontSize: 14),
+                ),
+                const SizedBox(height: 16),
+
+                _buildSectionTitle('Job Type:'),
+                Text(
+                  jobData['job type'] ?? 'No job type',
+                  style: const TextStyle(fontSize: 14),
+                ),
+                const SizedBox(height: 16),
+
+                _buildSectionTitle('Location:'),
+                Text(
+                  jobData['location'] ?? 'No location',
+                  style: const TextStyle(fontSize: 14),
+                ),
+                const SizedBox(height: 16),
+
+                _buildSectionTitle('Created At:'),
+                Text(
+                  formattedDate,
+                  style: const TextStyle(fontSize: 14),
+                ),
+                const SizedBox(height: 16),
+
+                _buildSectionTitle('Number of Applicants:'),
+                Text(
+                  '${jobData['number of applicants'] ?? 0}',
+                  style: const TextStyle(fontSize: 14),
+                ),
+                const SizedBox(height: 16),
+
+                // Enabled (Job status)
+                _buildSectionTitle('Job Enabled:'),
+                Text(
+                  jobData['enabled'] == true ? 'Yes' : 'No',
+                  style: const TextStyle(fontSize: 14),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _buildSectionTitle(String title) {
+    return Text(
+      title,
+      style: const TextStyle(
+        fontSize: 16,
+        fontWeight: FontWeight.bold,
+      ),
+    );
+  }
+}
--- lib/services/fire_database.dart
+++ lib/services/fire_database.dart
@@ -6,7 +6,7 @@ class FireDatabase {
   FireDatabase._();
 
   static final _firebaseFirestore = FirebaseFirestore.instance;
-  static Future<void> addData({
+  static Future<DocumentReference<Map<String, dynamic>>?> addData({
     required String collection,
     String? doc,
     required Map<String, dynamic> data,
@@ -15,11 +15,13 @@ class FireDatabase {
       if (doc != null) {
         await _firebaseFirestore.collection(collection).doc(doc).set(data);
       } else {
-        await _firebaseFirestore.collection(collection).add(data);
+        // add data with auto generated id
+        return await _firebaseFirestore.collection(collection).add(data);
       }
     } catch (e) {
       debugPrint(e.toString());
     }
+    return null;
   }
 
   static Future<void> updateData(
@@ -40,49 +42,48 @@ class FireDatabase {
   }
 
   static Future<dynamic> getData(
-    String collection, {
-    String? orderBy,
-    bool descending = false,
-    // List of filters
-    // example: [['field', 'value', OperationFilter.isEqualTo.name],]
-    List<List<dynamic>>? filters,
-    // for pagination 
-    int? limit,
-    dynamic startAfterValue,
-    dynamic endBeforeValue,
-    bool isPrevious = false,
-  }) async {
+      String collection, {
+        String? orderBy,
+        bool descending = false,
+        // List of filters
+        // example: [['field', 'value', OperationFilter.isEqualTo.name],]
+        List<List<dynamic>>? filters,
+        // for pagination
+        int? limit,
+        dynamic startAfterValue,
+        dynamic endBeforeValue,
+        bool isPrevious = false,
+      }) async {
     try {
       Query<Map<String, dynamic>> query =
-          _firebaseFirestore.collection(collection);
+      _firebaseFirestore.collection(collection);
 
       // Apply ordering
       if (orderBy != null) {
         query = query.orderBy(orderBy, descending: descending);
       }
 
-      // Apply filters
       if (filters != null) {
         for (final filter in filters) {
           query = query.where(
             filter[0],
             isEqualTo:
-                filter[2] == OperationFilter.isEqualTo.name ? filter[1] : null,
+            filter[2] == OperationFilter.isEqualTo.name ? filter[1] : null,
             isLessThan:
-                filter[2] == OperationFilter.isLessThan.name ? filter[1] : null,
+            filter[2] == OperationFilter.isLessThan.name ? filter[1] : null,
             isLessThanOrEqualTo:
-                filter[2] == OperationFilter.isLessThanOrEqualTo.name
-                    ? filter[1]
-                    : null,
+            filter[2] == OperationFilter.isLessThanOrEqualTo.name
+                ? filter[1]
+                : null,
             isGreaterThan: filter[2] == OperationFilter.isGreaterThan.name
                 ? filter[1]
                 : null,
             isGreaterThanOrEqualTo:
-                filter[2] == OperationFilter.isGreaterThanOrEqualTo.name
-                    ? filter[1]
-                    : null,
+            filter[2] == OperationFilter.isGreaterThanOrEqualTo.name
+                ? filter[1]
+                : null,
             whereIn:
-                filter[2] == OperationFilter.whereIn.name ? filter[1] : null,
+            filter[2] == OperationFilter.whereIn.name ? filter[1] : null,
             arrayContains: filter[2] == OperationFilter.arrayContains.name
                 ? filter[1]
                 : null,
@@ -94,7 +95,7 @@ class FireDatabase {
                 ? filter[1]
                 : null,
             whereNotIn:
-                filter[2] == OperationFilter.whereNotIn.name ? filter[1] : null,
+            filter[2] == OperationFilter.whereNotIn.name ? filter[1] : null,
           );
         }
       }
@@ -122,7 +123,6 @@ class FireDatabase {
       return await query.get();
     } catch (e) {
       debugPrint(e.toString());
-
       return null;
     }
   }
@@ -130,7 +130,7 @@ class FireDatabase {
   static Future<dynamic> getDocument(String collection, String id) async {
     try {
       final data =
-          await _firebaseFirestore.collection(collection).doc(id).get();
+      await _firebaseFirestore.collection(collection).doc(id).get();
       return data.data();
     } catch (e) {
       debugPrint(e.toString());
