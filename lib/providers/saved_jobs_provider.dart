1004aa4c2e1ff484572387e4e41c3b7a8221c634
Salma Abdelaziz <salmayaraaa@gmail.com>
2024-10-18 16:13:34 +0300

    update saved provider

--- lib/providers/saved_jobs_provider.dart
+++ lib/providers/saved_jobs_provider.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class JobSaveProvider with ChangeNotifier {
   final List<String> _savedJobs = [];
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final String _docId = 'saved_jobs';
+
+  JobSaveProvider() {
+    _loadSavedJobs();
+  }
 
   bool isJobSaved(String jobId) {
     return _savedJobs.contains(jobId);
@@ -13,8 +20,24 @@ class JobSaveProvider with ChangeNotifier {
     } else {
       _savedJobs.add(jobId);
     }
+    _saveJobs();
     notifyListeners();
   }
 
   List<String> get savedJobs => _savedJobs;
+
+  void _saveJobs() async {
+    await _firestore.collection('jobs').doc(_docId).set({
+      'savedJobs': _savedJobs,
+    });
+  }
+
+  void _loadSavedJobs() async {
+    DocumentSnapshot doc = await _firestore.collection('jobs').doc(_docId).get();
+    if (doc.exists) {
+      List<String> savedJobs = List<String>.from(doc['savedJobs']);
+      _savedJobs.addAll(savedJobs);
+      notifyListeners();
+    }
+  }
 }
